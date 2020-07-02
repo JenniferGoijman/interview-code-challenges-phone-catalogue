5a169a4da6a1c8da4249eb164d7ceb1ab63cdce6
JenniferGoijman <jgoijman@gmail.com>
2020-07-02 18:57:37 +0200

    add breadcrumbs at Home

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import './Home.scss';
 import { Spin } from 'antd';
+import { Breadcrumb } from 'antd';
 import { getAll } from '../../redux/actions/phones';
 import PhoneInCatalogue from '../../components/PhoneInCatalogue/PhoneInCatalogue';
 
@@ -11,11 +12,18 @@ const Home = props => {
     }, [])
 
     return (
-        <div className="home-container">
-            {!props.phones && <Spin size="large" />}
-            {props.phones?.length > 0 && props.phones?.map(phone => 
-                <PhoneInCatalogue key={phone.id} phone={phone} />
-            )}
+        <div>
+            <Breadcrumb>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>Phones</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <div className="phones-container">
+                {!props.phones && <Spin size="large" />}
+                {props.phones?.length > 0 && props.phones?.map(phone => 
+                    <PhoneInCatalogue key={phone.id} phone={phone} />
+                )}
+            </div>
         </div>
     )
 }
