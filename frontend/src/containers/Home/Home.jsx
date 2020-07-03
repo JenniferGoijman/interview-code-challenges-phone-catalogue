c5110dd1290fcc467e4bec4300803719ab8e3049
JenniferGoijman <jgoijman@gmail.com>
2020-07-03 12:07:37 +0200

    add styles to make it responsive

--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -17,6 +17,11 @@ const Home = props => {
     props.phones.map(phone => { const color = phone['color']; 
         if (!colors.includes(color)) {colors.push(color)}}) //Order by color
     const [colorsSelected, setColorsSelected] = useState(colors);
+    
+    const manufacturers = []
+    props.phones.map(phone => { const manufacturer = phone['manufacturer']; 
+        if (!manufacturers.includes(manufacturer)) {manufacturers.push(manufacturer)}}) //Order by manufacturer
+    const [manufacturersSelected, setManufacturersSelected] = useState(manufacturers);
 
     useEffect(() => {
         getAll();
@@ -35,7 +40,11 @@ const Home = props => {
 
     function handleChangeColor(value) {
         setColorsSelected(value);
-      }
+    }
+
+    function handleChangeManufacturer(value) {
+        setManufacturersSelected(value);
+    }
 
     return (
         <div className="home-container">
@@ -47,15 +56,26 @@ const Home = props => {
             </div>
             
             {props.phones?.length > 0 && <div className="filters">
-                <div className="filter-price">
-                    <div className="price-label">Filter by price:</div>
-                    From <InputNumber style={{ margin: '0 16px' }} value={minPriceSelected} onChange={onChangeMinPrice} /> 
-                    to <InputNumber style={{ margin: '0 16px' }} value={maxPriceSelected} onChange={onChangeMaxPrice} />
-                    <Slider range step={1} min={minPrice} max={maxPrice} defaultValue={[minPrice, maxPrice]} 
-                        defaultValue={[minPriceSelected, maxPriceSelected]} onChange={onChangePrice} />
+                <div className="filter">
+                    <div className="filter-label">Filter by brand:</div>
+                    <Select mode="multiple" style={{ width: 320 }} placeholder="Please select" 
+                        defaultValue={manufacturersSelected} onChange={handleChangeManufacturer} >
+                        {manufacturers.map(manufacturer => <Option key={manufacturer}>{manufacturer}</Option>)}
+                    </Select>
                 </div>
-                <div className="filter-color">
-                    <div className="color-label">Filter by color:</div>
+                <div className="filter">
+                    <div className="filter-label">Filter by price:</div>
+                    <div>
+                        From <InputNumber min={minPrice} max={maxPrice} value={minPriceSelected} 
+                            onChange={onChangeMinPrice} formatter={value => `${value} €`} style={{ margin: '0 16px' }} /> 
+                        to <InputNumber min={minPrice} max={maxPrice} value={maxPriceSelected} 
+                            onChange={onChangeMaxPrice} formatter={value => `${value} €`} style={{ margin: '0 16px' }} />
+                    </div>
+                    <Slider range step={1} min={minPrice} max={maxPrice} defaultValue={[minPrice, maxPrice]} 
+                        value={[minPriceSelected, maxPriceSelected]} onChange={onChangePrice} style={{ minWidth:250 }} />
+                </div>
+                <div className="filter">
+                    <div className="filter-label">Filter by color:</div>
                     <Select mode="multiple" style={{ width: 270 }} placeholder="Please select" 
                         defaultValue={colorsSelected} onChange={handleChangeColor} >
                         {colors.map(color => <Option key={color}>{color.charAt(0).toUpperCase() + color.slice(1)}</Option>)}
@@ -67,7 +87,8 @@ const Home = props => {
                 <div className="phone-container">
                     {!props.phones && <Spin size="large" />}
                     {props.phones?.length > 0 && props.phones?.filter(phone => 
-                        phone.price >= minPriceSelected && phone.price <= maxPriceSelected && colorsSelected.includes(phone.color))
+                        phone.price >= minPriceSelected && phone.price <= maxPriceSelected && 
+                        colorsSelected.includes(phone.color) && manufacturersSelected.includes(phone.manufacturer))
                         .map(phone => <PhoneInCatalogue key={phone.id} phone={phone}/>)}
                 </div>
             </div>
