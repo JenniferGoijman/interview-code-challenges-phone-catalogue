c0872bff9dd85efdd7dc5b219ecebf1b028cad52
JenniferGoijman <jgoijman@gmail.com>
2020-07-03 15:55:49 +0200

    add use history to redirect

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Result, Button } from 'antd';
 
 const NotFound = () => {
+    const history = useHistory();
+
+    const goHome = () => {
+        history.push('/');
+    }
+
     return (
         <Result 
             status="404"
             title="404" 
             subTitle="Sorry, the page you visited does not exist."
-            extra={<Button type="primary" href="/">Back Home</Button>}
+            extra={<Button type="primary" onClick={goHome}>Back Home</Button>}
         />
     )
 }
--- frontend/src/containers/Phone/Phone.jsx
+++ frontend/src/containers/Phone/Phone.jsx
@@ -1,27 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './Phone.scss';
 import { Spin, Breadcrumb } from 'antd';
 import PhoneDetails from '../../components/PhoneDetails/PhoneDetails';
 import NotFound from '../../components/NotFound/NotFound';
 
 const Phone = props => {
-    const [currentPhone, setCurrentPhone] = useState();
+    const history = useHistory();
+    const [currentPhone, setCurrentPhone] = useState(0);
+
     useEffect(() => {
         setCurrentPhone(props.phones?.find(phone => phone.id == props.match.params.id));
     }, [])
 
+    const goHome = () => {
+        history.push('/');
+    }
+
     return (
         <div className="phone-container">
-            {props.phones?.length>0 && !currentPhone && <NotFound />}
+            {props.phones?.length>0 && !currentPhone && currentPhone !== 0 && <NotFound />}
             
-            {!props.phones?.length>0 && !currentPhone && <Spin size="large" />}
+            {!props.phones?.length>0 && currentPhone === 0 && <Spin size="large" />}
             
             {currentPhone && <div className="phone">
                 <div className="breadcrumb">
                     <Breadcrumb>
                         <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item><a href="/">Phones</a></Breadcrumb.Item>
+                        <Breadcrumb.Item><a onClick={goHome}>Phones</a></Breadcrumb.Item>
                         <Breadcrumb.Item>{currentPhone?.name}</Breadcrumb.Item>
                     </Breadcrumb>
                 </div>
